fc73074c661d38ea66f03f71aacfa4d87fd1c0cf
GOKULRAJ-B <bbgokulrajb@gmail.com>
2026-02-04 17:10:06 +0530

    problems on linked list

--- Linked_List/circular.cpp
+++ Linked_List/circular.cpp
@@ -42,6 +42,74 @@ void insertAtBegining(Node* &head, int val){
     head = newnode;
 }
 
+void deleteAtend(Node* &head){
+    if(head==NULL) return;
+    Node* temp = head;
+    if(head->next == head){
+        delete head;
+        head = NULL;
+        return;
+    }
+    while(temp->next->next!=head){
+        temp = temp->next;
+    }
+    Node* toDel = temp->next->next;
+    temp->next = head;
+    delete toDel;
+}
+
+int countOfEven(Node* head){
+    if(head == NULL){
+        return 0;
+    }
+    int count=0;
+    Node* temp = head;
+    if(temp->data % 2 == 0) count++;
+    temp = temp->next;
+    while(temp!=head){
+        if(temp->data % 2 == 0) count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+
+
+void deleteAtBegining(Node* &head){
+    if(head==NULL) return;
+
+    if(head->next == head){
+        delete head;
+        head = NULL;
+        return;
+    }
+
+    Node* temp = head;
+    while(temp->next != head){
+        temp = temp->next;
+    }
+    Node* toDel = head;
+    temp->next = head->next;
+    head = head->next;
+    delete toDel;
+}
+
+void deleteAtN(Node* &head, int pos){
+    if(head==NULL) return;
+    if(pos == 1){
+        deleteAtBegining(head);
+        return;
+    }
+    Node* temp = head;
+    for(int i=1;i<pos-1 && temp->next->next!=head; i++){
+        temp = temp->next;
+    }
+    Node* toDel = temp->next;
+    temp->next = temp->next->next;
+    delete toDel;
+
+}
+
 void printList(Node* &head){
     if(head==NULL) return;
     Node* temp = head->next;
@@ -54,12 +122,21 @@ void printList(Node* &head){
 
 int main(){
     Node* head = NULL;
-    //insertAtend(head,1);
-    //insertAtend(head,2);
-    //insertAtend(head,3);
-    insertAtBegining(head,1);
-    insertAtBegining(head,2);
-    insertAtBegining(head,3);
-    insertAtBegining(head,4);
+    insertAtend(head,1);
+    insertAtend(head,2);
+    insertAtend(head,6);
+    insertAtend(head,4);
+    //insertAtBegining(head,1);
+    //insertAtBegining(head,2);
+    //insertAtBegining(head,3);
+    //insertAtBegining(head,4);
+    //printList(head);
+    //deleteAtBegining(head);
+    //cout<<endl;
     printList(head);
+    cout<<endl;
+    //deleteAtend(head);
+    //deleteAtN(head, 3);
+    //printList(head);
+    cout<<countOfEven(head);
 }
--- Linked_List/single_linked_list.cpp
+++ Linked_List/single_linked_list.cpp
@@ -101,16 +101,47 @@ void deleteAtBegining(Node* &head){
     head = temp;
 }
 
+void deleteAtN(Node* &head, int pos){
+    if(head==NULL) return;
+    Node* temp = head;
+    if(pos == 1){
+        Node* toDel = head;
+        head = head->next;
+        delete toDel;
+        return;
+    }
+    for(int i=1; i<pos-1 && temp->next!=NULL; i++){
+        temp = temp->next;
+    }
+    if(temp == NULL || temp->next == NULL) return;
+    Node* toDel = temp->next;
+    temp->next = temp->next->next;
+    delete toDel;
+}
+
+bool searchRecursiveKey(Node* head, int key){
+    if(head->data==key){
+        return true;
+    }
+    if(head->next==NULL || head==NULL){
+        return false;
+    }
+    return searchRecursiveKey(head->next,key);
+
+}
+
 int main(){
     Node* head = NULL;
-    insertAtEnd(head,1);
+    //insertAtEnd(head,1);
     insertAtBegining(head,5);
     insertAtBegining(head,6);
     insertAtBegining(head,7);
-    insertAtN(head, 0, 15);
+    //insertAtN(head, 0, 15);
     listPrint(head);
     //deleteLastNode(head);
-    deleteAtBegining(head);
+    //deleteAtBegining(head);
+    //deleteAtN(head,1);
     cout<<endl;
-    listPrint(head);
+    //listPrint(head);
+    cout<<((searchRecursiveKey(head,55))?"Found":"Not Found");
 }

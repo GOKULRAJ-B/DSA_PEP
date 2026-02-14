8b5ce6f495e5efab8a69e04e04e39bc180656254
GOKULRAJ-B <bbgokulrajb@gmail.com>
2026-02-14 22:36:08 +0530

    trees started

---
+++ Trees/01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+void inorder(Node* root, int &count){
+        if(root == NULL) return;
+        inorder(root->left,count);
+        count++;
+        inorder(root->right,count);
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    int count = 0;
+    inorder(root,count);
+    cout<<count;
+}
